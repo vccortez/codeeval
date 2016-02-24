90f0fc11b3cae3d00f73fca5fd08508a73a7f4ec
Vitor Cortez <vitor.a.cortez@gmail.com>
2016-02-24 14:42:21 -0300

    Remove ignored files

--- moderate/email-validation/lua-email-validation.lua
+++ moderate/email-validation/lua-email-validation.lua
@@ -1,6 +1,8 @@
-for line in io.lines('in.txt') do
+for line in io.lines(arg[1]) do
   if #line > 0 then
-    if not line:match("[%p%s][%p%s]") and line:match("^[%w_%.%+%-]+@[%w]+%.[%a]+$") then
+    if
+      not line:match("[%p%s][%p%s]")
+      and line:match("^[%w_%.%+%-]+@[%w][%w]+[%.%w]*%.[%a]+$") then
       io.write('true\n')
     else
       io.write('false\n')
